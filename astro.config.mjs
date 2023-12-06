274eb8ead7677bb8c00b04057aa095a87924a30d
Jan <ja.dorn@web.de>
2023-12-06 20:55:13 +0100

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vue from "@astrojs/vue";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    vue(),
-  ]
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), vue(), sitemap()]
 });
