56bdeb2a32d4232d35348f72d6cb42bcbe7c0da8
Jan <ja.dorn@web.de>
2023-12-07 15:47:01 +0100

    AnimationCard template now with props

--- src/components/AnimationCard.vue
+++ src/components/AnimationCard.vue
@@ -1,7 +1,41 @@
 <script setup lang="ts">
+import { ref, watchEffect } from 'vue';
+
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { Badge } from '@/components/ui/badge'
+
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import 'highlight.js/styles/github-dark.css';
+
+hljs.registerLanguage('javascript', javascript);
+
+const { animationTitle, linkText, linkUrl, code } = defineProps({
+  animationTitle: {
+    type: String,
+    required: true
+  },
+  linkText: {
+    type: String,
+    required: true
+  },
+  linkUrl: {
+    type: String,
+    required: true
+  },
+  code: {
+    type: String,
+    required: true
+  }
+});
+
+const highlightedCode = ref('');
+
+watchEffect(() => {
+  highlightedCode.value = hljs.highlight(code, { language: 'javascript' }).value;
+});
+
 </script>
 
 <template>
@@ -18,24 +52,21 @@ import { Badge } from '@/components/ui/badge'
       <Card>
         <CardHeader>
           <CardTitle>
-            Animation Title 
-            <a href="https://gsap.com/docs/v3/Plugins/ScrollTrigger/" target="_blank">
-              <Badge class="ml-4 rounded-full" variant="outline">ScrollTrigger</Badge>
+            {{ animationTitle }} 
+            <a :href="linkUrl" target="_blank">
+              <Badge class="ml-4 rounded-full" variant="outline">{{ linkText }} </Badge>
             </a>
           </CardTitle>
         </CardHeader>
         <CardContent class="space-y-2">
-          Content
+          <slot />
         </CardContent>
-        <!-- <CardFooter>
-          Footer
-        </CardFooter> -->
       </Card>
     </TabsContent>
     <TabsContent value="code">
       <Card class="bg-[#394D3F] text-[#cfd1d5]">
         <CardContent class="m-4 p-0 font-mono text-sm font-light">
-          Code
+          <div v-html=highlightedCode></div>
         </CardContent>
       </Card>
     </TabsContent>
