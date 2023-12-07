9ece1c9d96424a2b9f94b54b44141ecb76dc7798
Jan <ja.dorn@web.de>
2023-12-07 21:02:34 +0100

    center content

--- src/components/AnimationCard.vue
+++ src/components/AnimationCard.vue
@@ -58,7 +58,7 @@ watchEffect(() => {
             </a>
           </CardTitle>
         </CardHeader>
-        <CardContent class="space-y-2">
+        <CardContent class="flex justify-center space-y-2">
           <slot />
         </CardContent>
       </Card>
