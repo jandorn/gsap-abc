95979488219b86069523753a9891a4ff211351b2
Jan <ja.dorn@web.de>
2023-12-09 15:45:51 +0100

    added key and rm scrollbar from code block

--- src/components/AnimationCard.vue
+++ src/components/AnimationCard.vue
@@ -18,10 +18,14 @@ const { animationTitle, linkText, linkUrl } = defineProps({
   }
 });
 
+import { ref } from 'vue';
+
+const slotKey = ref(0);
+
 </script>
 
 <template>
-  <Tabs default-value="animation" class="w-full mb-16">
+  <Tabs default-value="animation" class="w-full mb-16" @update:model-value="slotKey++, console.log(slotKey)">
     <TabsList class="grid w-48 grid-cols-2">
       <TabsTrigger value="animation">
         Animation
@@ -30,7 +34,7 @@ const { animationTitle, linkText, linkUrl } = defineProps({
         Code
       </TabsTrigger>
     </TabsList>
-    <TabsContent class="overflow-hidden" value="animation">
+    <TabsContent class="overflow-hidden" value="animation" :key="slotKey">
       <Card>
         <CardHeader>
           <CardTitle>
@@ -47,10 +51,23 @@ const { animationTitle, linkText, linkUrl } = defineProps({
     </TabsContent>
     <TabsContent value="code">
       <Card class="bg-[#2F343F] text-[#cfd1d5]">
-        <CardContent class="max-h-[25rem] overflow-scroll m-4 p-0 font-mono text-sm font-light">
+        <CardContent class="max-h-[25rem] overflow-scroll m-4 p-0 font-mono text-sm font-light scrollbar-hide">
           <slot name="code" />
         </CardContent>
       </Card>
     </TabsContent>
   </Tabs>
 </template>
+
+<style>
+/* For Webkit-based browsers (Chrome, Safari and Opera) */
+.scrollbar-hide::-webkit-scrollbar {
+  display: none;
+}
+
+/* For IE, Edge and Firefox */
+.scrollbar-hide {
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+}
+</style>
